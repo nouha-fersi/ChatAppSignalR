0c04e90696a72814178d270c8ec3e7459b843390
nouha-fersi <nouha.fersi@ensi-uma.tn>
2024-12-25 15:35:29 +0300

    Initial commit

---
+++ DemoChatApp/DemoChatApp/ChatHubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using ChatModels;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DemoChatApp.ChatHubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Chat chat)
+            => await Clients.All.SendAsync("ReceiveMessage",chat);
+    }
+}
